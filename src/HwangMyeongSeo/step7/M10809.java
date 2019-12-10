965325aef098758694f8d5aea86244dcf187debb
HJS <HJS@DESKTOP-IAF92FM>
2019-12-10 23:37:31 +0900

    test

--- src/HwangMyeongSeo/step7/M10809.java
+++ src/HwangMyeongSeo/step7/M10809.java
@@ -7,7 +7,7 @@ public class M10809 {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
-		String input = sc.next();
+		String input = sc.next(); 
 		for(char c = 'a'; c<='z';c++) System.out.print(input.indexOf(c)+" "); 
 	}
 
